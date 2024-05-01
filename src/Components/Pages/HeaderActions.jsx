a3ca6545c5f7b9ac265d0d6db94a263a8ada350c
Hatsumi <burdenjuk.bohdan.clg@chnu.edu.ua>
2024-05-01 14:43:47 +0200

    Commit 01.05.24/14:42 Spain-Time (Update Trafic Light: Add aminations & settings animations)

--- src/Components/Pages/HeaderActions.jsx
+++ src/Components/Pages/HeaderActions.jsx
@@ -3,3 +3,22 @@ export const AddComponent = (Component) => ({
     Component: Component
   });
 
+export const SET_BRIGHTNESS = (brightness) => ({
+  type: 'SET_BRIGHTNESS',
+  brightness: brightness
+});
+
+export const SET_TRASITION_TIME = (trasition_time) => ({
+  type: 'SET_TRASITION_TIME',
+  trasition_time: trasition_time
+});
+
+export const SET_TIME_ATIMATION = (TIME) => ({
+  type: 'SET_TIME',
+  TIME: TIME
+});
+
+export const ENABLE_OPTIONS = () => ({
+  type: 'ENABLE_OPTIONS_TRAFIC',
+
+});
